996a91ff08769b536b65ef5ffda5568c5c753cb7
Sam Bisciglia <sbiscigl@asu.edu>
2017-09-18 11:44:32 -0500

    flattened ifs

--- requesthandler/request.go
+++ requesthandler/request.go
@@ -32,49 +32,51 @@ func (rh *RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		/*return 503 and response with information*/
 		w.WriteHeader(503)
 		w.Write(entities.NewResponse(503, "there are no healthy servers").ToJSON())
-	} else {
-		req, err := http.NewRequest(r.Method, "http://"+s.Host, r.Body)
-		if err != nil {
-			log.Println("could not create request")
-		}
-		/*Loop through headers*/
-		for name, headers := range r.Header {
-			name = strings.ToLower(name)
-			for _, h := range headers {
-				req.Header.Add(name, h)
-			}
-		}
-
-		/*record in use*/
-		rh.servers.EditUsage(s.Host, 1)
-		/*execute request*/
-		resp, err := rh.client.Do(req)
-		/*record out of use*/
-		rh.servers.EditUsage(s.Host, -1)
-
-		if err != nil {
-			log.Println("error in requesting endpoint -- server most likely offline" +
-				" removing " + s.Host + " from available servers")
-			/*Remove server from server list*/
-			rh.servers.RemoveServerAddresses(s.Host)
-			/*retry*/
-			rh.ServeHTTP(w, r)
-		} else {
-			if resp.StatusCode >= 500 && resp.StatusCode < 600 {
-				/*set instance to unhealthy*/
-				rh.servers.SetHealth(s.Host, false)
-				/*retry*/
-				rh.ServeHTTP(w, r)
-			} else {
-				defer resp.Body.Close()
-				read, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Println("error reading body")
-				}
-
-				w.WriteHeader(resp.StatusCode)
-				w.Write(read)
-			}
+		return
+	}
+	req, err := http.NewRequest(r.Method, "http://"+s.Host, r.Body)
+	if err != nil {
+		log.Println("could not create request")
+	}
+	/*Loop through headers*/
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			req.Header.Add(name, h)
 		}
 	}
+
+	/*record in use*/
+	rh.servers.EditUsage(s.Host, 1)
+	/*execute request*/
+	resp, err := rh.client.Do(req)
+	/*record out of use*/
+	rh.servers.EditUsage(s.Host, -1)
+
+	if err != nil {
+		log.Println("error in requesting endpoint -- server most likely offline" +
+			" removing " + s.Host + " from available servers")
+		/*Remove server from server list*/
+		rh.servers.RemoveServerAddresses(s.Host)
+		/*retry*/
+		rh.ServeHTTP(w, r)
+		return
+	}
+
+	if resp.StatusCode >= 500 && resp.StatusCode < 600 {
+		/*set instance to unhealthy*/
+		rh.servers.SetHealth(s.Host, false)
+		/*retry*/
+		rh.ServeHTTP(w, r)
+		return
+	}
+
+	defer resp.Body.Close()
+	read, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("error reading body")
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	w.Write(read)
 }
